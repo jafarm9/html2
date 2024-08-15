cb7c15f05ea47b84945394c073e6da5564756022
jafarm9 <mahmoodnezhadjafar@gmail.com>
2024-08-15 13:57:48 +0330

    part#7

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,4 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {});
+});
+
+
+const addUserButton = document.getElementById('add-user-button')
+addUserButton.addEventListener('click', () => {
+  document.querySelector('.add-User-modal').classList.add('show')
+  document.querySelector('.add-User-modal-black').classList.remove('dis-none')
+});
+
+document.querySelector('.add-User-modal-black').addEventListener('click' , (e) =>{
+  document.querySelector('.add-User-modal').classList.remove('show')
+  e.target.classList.add('dis-none')
+});
